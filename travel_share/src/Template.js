6183ff7a6c8b16e2c24d76b7e70d20084aa5defe
Kavya Ravikanti <Kavya@Kavyas-MBP.lan>
2017-05-26 15:57:51 -0400

    completed structure of sidebar

--- travel_share/src/Template.js
+++ travel_share/src/Template.js
@@ -7,55 +7,80 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 injectTapEventPlugin();
 require('./Template.css');
 
-const style = {
-  height: 1000,
-  width: 1000,
-  margin: 20,
-  textAlign: 'center',
-  display: 'inline-block',
-};
 
-const PaperPanel = () => (
-  <div>
-    <Paper style={style} zDepth={2} />
-  </div>
+// style for button
+const style = {
+  marginRight: 20,
+}; 
+
+
+const AddButton = () => (
+    <div>
+    <FloatingActionButton mini={true} style={style}>
+      <ContentAdd />
+    </FloatingActionButton>
+    </div>
 );
 
 export class TypeSelector extends Component {
     constructor(props) {
         super(props);
-        this.state = {value: 1, primaryText: "Flight"};
+        this.state = {
+            value: 1, 
+            primaryText: "Flight",
+            inputText: ""
+        };
     }
 
-    handleChange = (event, index, value, primaryText) => this.setState({value, primaryText});
+    handleMenuChange = (event, index, value) => this.setState({value});
+
+    handleInput = (event, index, inputText) => this.setState({inputText});
 
     render() {
         return (
+        <div>
         <div className="selector">
+            <h3> Select Aspect Of Itinerary: </h3> 
+            <div>
             <SelectField 
                 floatingLabelText = "Aspect"
                 value={this.state.value} 
-                primaryText={this.state.primaryText}
-                onChange={this.handleChange}>
+                onChange={this.handleMenuChange}>
                 <MenuItem value={1} primaryText={"Flight"} />
                 <MenuItem value={2} primaryText={"Transportation"} />
-                <MenuItem value={3} primaryText={"Lodging"} />
+                {/*<MenuItem value={3} primaryText={"Lodging"} />
                 <MenuItem value={4} primaryText={"Day 1"} />
-                <MenuItem value={5} primaryText={"Day 2"} />
+                <MenuItem value={5} primaryText={"Day 2"} />*/}
             </SelectField>
+            </div>
             <br />
-            <TextField hintText="Enter here" />
+            <div >
+                <TextField hintText={"Enter Here"} value={this.state.inputText} onChange={this.handleInput}/>
+            </div>
             <br />
-            <RaisedButton label="Submit"  />    
+            <RaisedButton label="Submit"  />   
+            <h3> Add A Category </h3>
+            <TextField hintText="Enter New Category"  />
+            <AddButton />
         </div>
+        <div className="display">
+             <Card>
+                <CardTitle subtitle="information" />
+                <CardText> {this.state.inputText} </CardText>
+             </Card> 
+        </div> 
+        </div>   
         )
     }
 
 }
+
 
 export default class Template extends Component {    
     
@@ -63,13 +88,8 @@ export default class Template extends Component {
         return (
         <MuiThemeProvider>
             <div className ="template">
-            <div className="sidebar">
-            <h3> Select aspect of intinerary: </h3> 
-            <TypeSelector />
-            </div>
-            <div className="panel">
-                <PaperPanel />
-            </div>
+                    
+                    <TypeSelector />
             </div>
       </MuiThemeProvider>
     );
