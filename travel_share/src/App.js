4a8cf18e84dcd2c24badbe7d6af4d643759152b6
Kavya Ravikanti <Kavya@Kavyas-MBP.lan>
2017-06-06 08:42:32 -0400

    cleaned up console logs and white space

--- travel_share/src/App.js
+++ travel_share/src/App.js
@@ -22,9 +22,7 @@ class App extends Component {
           logInState: true
         }
   }     
-  // componentDidMount() {
-  //   window.location.reload(true);
-  // }
+  
   handleLogInButton(logInState) {
     if(this.state.logInState) {
         firebase.auth().signInWithPopup(provider).then((result) => {
@@ -34,23 +32,14 @@ class App extends Component {
           logInState: !this.state.logInState,
           userID: result.user.uid
         })
-      // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
-        // The signed-in user info.
         var user = result.user;
-        
-      // ...
         }).catch(function(error) {
-          // Handle Errors here.
           var errorCode = error.code;
           var errorMessage = error.message;
-          // The email of the user's account used.
           var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
           var credential = error.credential;
-          // ...
         });
-       
     }
     else {
       firebase.auth().signOut().then(() => {
@@ -59,20 +48,13 @@ class App extends Component {
           emailVerified: false,
           logInState: !this.state.logInState
         })
-        console.log("log out successful")
-        
         window.location = "/";
-
       }).catch(function(error) {
-        // An error happened.
       });
     }
-    
   }
 
   render() {
-    console.log(this.state.userID) 
-    console.log(this.state.logInState)
     return (
      
     <Router>
